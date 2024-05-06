52dda3439f5e1a3027e1625b2f6847b9978ce652
Meng Xipeng <mengxipeng@gmail.com>
2024-05-06 18:13:57 +0800

    can show the list of files in the dumps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
 
 // src/index.ts
-function helloWorld() {
-    console.log('Hello, world!!! ok');
-}
 
-helloWorld();
+// read the a file `dumps.json`
+const dumpsJsonUrl = '/dumps.json';
+const appElement = document.getElementById('app');
+if (appElement) {
+
+
+    fetch(dumpsJsonUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error " + response.status);
+            }
+            return response.json();
+        })
+        .then((json : string[])=> {
+            console.log(json);
+            const ul = document.createElement('ul');
+            json.forEach(item => {
+                const li = document.createElement('li');
+                li.textContent = item;
+                ul.appendChild(li);
+            });
+            appElement.appendChild(ul);
+
+        })
+        .catch(function () {
+            console.log("An error occurred while fetching the JSON.");
+        });
+
+
+
+}
 
 
